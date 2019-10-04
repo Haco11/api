7a88584989cb03715d6d101f3c6acac8e08115ca
Haci11 <46707887+Haci11@users.noreply.github.com>
2019-10-04 22:43:33 +0200

    m
    m

--- rout/server.js
+++ rout/server.js
@@ -5,4 +5,4 @@ const port = 3000
 app.get('/Comments.js', (req, res) => res.send('Hello World!'))
 //här skapar man servern med port 3000
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log('Listening on port ${port}!`))
